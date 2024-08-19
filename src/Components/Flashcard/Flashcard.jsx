3d382b12d99368cabe40b0252097b7790d647dfb
dtron500 <daniel.mitchell3005@gmail.com>
2024-08-19 13:29:58 +0100

    Added card

--- src/Components/Flashcard/Flashcard.jsx
+++ src/Components/Flashcard/Flashcard.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react"
 import styles from "./Flashcard.module.css"
-
 export default function Flashcard () {
-    return (
-            <div className={styles.question}>
+    const [isClicked, setisClicked] = useState(false);
+    function handleClick(){
+        console.log('(.)(.)')
+    }
+    
+    return ( <>
+            <div onClick = {handleClick} className={styles.question}>
                 <p className={styles.cross}>❌</p>
                 <div className={styles.textBox}>
                     <p className={styles.emoji}>🤔</p>
                     <h3 className={styles.questionText}>What is JSX?</h3>
                 </div>
             </div>
-    )
-}
+            {/* /* // Answer card below */ }
+            <div className={styles.answer}>
+                <p className={styles.cross}>❌</p>
+                <div className={styles.textBox}>
+                    <p className={styles.emoji}>🤔</p>
+                    <h3 className={styles.questionText}>What is JSX?</h3>
+                </div>
+            </div>
+            </>
+) 
+} 
