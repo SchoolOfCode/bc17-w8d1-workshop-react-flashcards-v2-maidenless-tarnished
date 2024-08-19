8e2f1190fb3a7758a80d6851fa0c3e1dc22c3d86
James O'Kane <orbiccode@gmail.com>
2024-08-19 12:12:38 +0100

    Flashcard

--- src/Components/Flashcard/Flashcard.jsx
+++ src/Components/Flashcard/Flashcard.jsx
@@ -1,9 +1,13 @@
+import styles from "./Flashcard.module.css"
+
 export default function Flashcard () {
     return (
-        <>
-            <p>🤔</p>
-            <h3>What is JSX?</h3>
-        </>
-        
+            <div className={styles.question}>
+                <p className={styles.cross}>❌</p>
+                <div className={styles.textBox}>
+                    <p className={styles.emoji}>🤔</p>
+                    <h3 className={styles.questionText}>What is JSX?</h3>
+                </div>
+            </div>
     )
 }
